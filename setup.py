8e332c0a8516e52f9c4bbcc407731fece45c80b3
Zymrael <mic.poli@hotmail.it>
2019-06-02 17:20:56 -0700

    org: requirements and setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyPDMP',
+      version='0.1.0',
+      description='Identification of PDMP',
+      url='https://github.com/massastrello/PDMP-ID/blob/master/README.md',
+      author='S. Massaroli, M. Poli',
+      author_email='',
+      license='MIT',
+      packages=['pyPDMP'],
+      zip_safe=False)
